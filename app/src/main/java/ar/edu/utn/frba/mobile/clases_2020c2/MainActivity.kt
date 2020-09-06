000e62354fbeaa80673ffd13854baf789a9e48b8
Juan Brito <juan.a.brito90@gmail.com>
2020-09-06 10:51:41 -0300

    Clase 03 - Practica - Layouts

--- app/src/main/java/ar/edu/utn/frba/mobile/clases_2020c2/MainActivity.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/clases_2020c2/MainActivity.kt
@@ -2,22 +2,31 @@ package ar.edu.utn.frba.mobile.clases_2020c2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.Fragment
 import ar.edu.utn.frba.mobile.clases_2020c2.ui.main.MainFragment
-import ar.edu.utn.frba.mobile.clases_2020c2.ui.main.MainViewModel
-import ar.edu.utn.frba.mobile.clases_2020c2.ui.main.MainViewModelResources
+import ar.edu.utn.frba.mobile.clases_2020c2.ui.status_update.StatusUpdateFragment
 import kotlinx.android.synthetic.main.main_activity.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MainFragment.OnFragmentInteractionListener, StatusUpdateFragment.OnFragmentInteractionListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         setSupportActionBar(toolbar)
         if (savedInstanceState == null) {
+            // Solo la primera vez que corre el activity
+            // Las demás el propio manager restaura todo como estaba
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.fragmentContainer, MainFragment.newInstance(getString(R.string.whatAreYouThinking)))
+                .commit()
         }
+    }
+
+    override fun showFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(null)
+            .setCustomAnimations(R.anim.fragment_push_enter, R.anim.fragment_push_exit, R.anim.fragment_pop_enter, R.anim.fragment_pop_exit)
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
 }
