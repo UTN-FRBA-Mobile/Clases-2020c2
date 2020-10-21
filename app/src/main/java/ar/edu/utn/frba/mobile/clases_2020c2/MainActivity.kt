f67c4dc37a059c5f3300f6e758b1b99362f866bb
Juan Brito <juan.a.brito90@gmail.com>
2020-10-21 10:01:15 -0300

    Clase 09 - Notificaciones

--- app/src/main/java/ar/edu/utn/frba/mobile/clases_2020c2/MainActivity.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/clases_2020c2/MainActivity.kt
@@ -1,11 +1,13 @@
 package ar.edu.utn.frba.mobile.clases_2020c2
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import ar.edu.utn.frba.mobile.clases_2020c2.ui.main.MainFragment
-import ar.edu.utn.frba.mobile.clases_2020c2.ui.main.MainViewModel
-import ar.edu.utn.frba.mobile.clases_2020c2.ui.main.MainViewModelResources
+import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,11 +15,41 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        setSupportActionBar(toolbar)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+        MyPreferences
+        setFirebaseTokenInView()
+
+        reloadButton.setOnClickListener { setFirebaseTokenInView() }
+
+        copyButton.setOnClickListener { copyTokenToClipboard() }
+
+        subscribeButton.setOnClickListener { subscribeToTopic() }
+    }
+
+    private fun setFirebaseTokenInView() {
+        val firebaseTokenText = MyPreferences.getFirebaseToken(this)
+
+        if (firebaseToken != null) {
+            firebaseToken.setText(firebaseTokenText)
+            reloadButton.visibility = View.GONE
+            copyButton.visibility = View.VISIBLE
+            topicContainer.visibility = View.VISIBLE
+        } else {
+            copyButton.visibility = View.GONE
+            topicContainer.visibility = View.GONE
+            reloadButton.visibility = View.VISIBLE
         }
+    }
+
+    private fun subscribeToTopic() {
+        val topicText = topic.text.toString()
+        FirebaseMessaging.getInstance().subscribeToTopic(topicText)
+        Toast.makeText(this, "Subscripto a $topicText", Toast.LENGTH_SHORT).show()
+        topic.setText("")
+    }
+
+    fun copyTokenToClipboard() {
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("Firebase token", firebaseToken.text)
+        clipboard.setPrimaryClip(clip)
     }
 }
